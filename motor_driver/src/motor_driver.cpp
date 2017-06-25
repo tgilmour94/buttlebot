d4e32e42b1e04faa82cc4cdb055c8b71d47170b4
Thomas <tgilmour94@gmail.com>
2017-06-24 19:18:44 -0700

    Motor Driver with working serial communications

--- motor_driver/src/motor_driver.cpp
+++ motor_driver/src/motor_driver.cpp
@@ -3,37 +3,49 @@
 #include <std_msgs/Empty.h>
 #include <serial/serial.h>
 #include <ros/console.h>
-//#include <string.h>
 
-#define	SOP			  0
-#define CMD			  1
-#define LEN			  2
+#define	SOP		0
+#define CMD		1
+#define LEN		2
 #define CTL_BYTE	3
 #define ANGVELH		6
 #define ANGVELL		7
 #define LINVELH		8
 #define LINVELL		9
-#define XOR			  12
+#define XOR		12
+#define KARTRADIUS      275
 
 int getCheckSum(volatile unsigned char *data, int length);
 
 serial::Serial ser;
+int linearVelocity = 0;
+int angularVelocity = 0;
 
 void CallBackMotorControl(const geometry_msgs::Twist& vel)
 {
- //  ser.write(MOVECOMMAND);
+/*
+linvelH is msb
+linvelL is lsb
+0x0000 = no speed 
+0x0001 = min backward speed
+0x0000 + (speed in mm/s converted to hex) = backward speed 
+0xffff - (speed in mm/s converted to hex) = forward speed 
+*/
+  linearVelocity = (int)vel.linear.x;
+  angularVelocity = -vel.angular.z*KARTRADIUS;
 }
+
 void CallBackBeeper(const std_msgs::Empty& msg)
 {
  // ser.write(BEEPERCODE);
 }
 
-int serialSetup(*data)
+int serialSetup(unsigned char *data)
 {
   try
   {
     ser.setPort("/dev/ttyUSB0");
-    ser.setBaudrate(9600);
+    ser.setBaudrate(115200);
     serial::Timeout to = serial::Timeout::simpleTimeout(1000);
     ser.setTimeout(to);
     ser.open();
@@ -56,12 +68,9 @@ int main(int argc, char **argv)
 {
   // Variables
 
-	unsigned char cmdSetup[] = { 0x55, 0xac, 0x09, 0x00, 0x00, 0x00, 0x00,
-						                   0x00, 0x00, 0x00, 0x00, 0x00, 0xF0};
-	unsigned char cmdGetData[] = { 0x55, 0xAB, 0x09, 0x00, 0x00, 0x00, 0x00,
-								                 0x00, 0x00, 0x00, 0x00, 0x00, 0xF7 };
-	unsigned char cmdSend[] = { 0x55, 0xAB, 0x09, 0x00, 0x00, 0x00, 0x00,
-								              0x00, 0x00, 0x00, 0x00, 0x00, 0xF7 };
+	unsigned char cmdSetup[] = { 0x55, 0xac, 0x09, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF0};
+	unsigned char cmdGetData[] = { 0x55, 0xAB, 0x09, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF7 };
+	unsigned char cmdSend[] = { 0x55, 0xAB, 0x09, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF7 };
 
   // Code Space
   ros::init(argc,argv,"motor_driver");
@@ -71,19 +80,71 @@ int main(int argc, char **argv)
   if(!serialSetup(&cmdSetup[0])) return 0;
   ros::Rate rate (10);
 
-  // Test - Turn on motors
-  cmdSend[LINVELL] = 0x10;
-  cmdSend[LINVELH] = 0x10;
-
   while(ros::ok())
   {
+    //Update Linear & Angular Velocity
+    int linQuotient;
+    int angQuotient;
+    if(linearVelocity > 0)
+    {
+      linQuotient = 65535 - linearVelocity;
+      angQuotient = 65535 - angularVelocity;
+    }
+    else 
+    {
+      linQuotient = -linearVelocity;
+      angQuotient = -angularVelocity;
+    }
+    unsigned char linTempChar = 0x00;
+    unsigned char angTempChar = 0x00;
+    for(int i=0;i<4;i++)
+    {
+      int linTemp = linQuotient%16;
+      int angTemp = angQuotient%16;
+      switch(i)
+      {
+        case 0:
+        cmdSend[ANGVELL] = 0x00 + angTemp;
+        cmdSend[LINVELL] = 0x00 + linTemp;
+        break;
 
+        case 1:
+        linTempChar = linTemp;
+        linTempChar <<=4;
+        cmdSend[LINVELL] = linTempChar | cmdSend[LINVELL];
+        angTempChar = angTemp;
+        angTempChar <<=4;
+        cmdSend[ANGVELL] = angTempChar | cmdSend[ANGVELL];       
+        break;
+
+        case 2:  
+        cmdSend[LINVELH] = 0x00 + linTemp;
+        cmdSend[ANGVELH] = 0x00 + angTemp;
+        break;
+     
+        case 3:
+        linTempChar = linTemp;
+        linTempChar <<=4;
+        cmdSend[LINVELH] = linTempChar | cmdSend[LINVELH]; 
+        angTempChar = angTemp;
+        angTempChar <<=4;
+        cmdSend[ANGVELH] = angTempChar | cmdSend[ANGVELH];
+        break;
+      }
+      linQuotient = linQuotient/16;
+      angQuotient = angQuotient/16;
+    } 
+   // ROS_INFO("%d, %d",cmdSend[LINVELH],cmdSend[LINVELL]);
+    //END Update Linear& Angular Velocity
+
+
+    //Send Command
     cmdSend[XOR] = getCheckSum(&cmdSend[0], 12);
     ser.write(&cmdSend[0], 13);
     ros::spinOnce();
     rate.sleep();
   }
-  ser.write(EXITCODE);
+  //ser.write(EXITCODE);
   return 0;
 
 }
